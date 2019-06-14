85606264ae710b6b00b2b6b22f2d86edb5254dc4
Shaohsuan Cheng <shaohsuan.cheng.tw@gmail.com>
2019-06-14 12:22:47 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 CUR_PATH=$(pwd)
 
-declare -a arr=(".vim" ".vimrc" ".gvimrc")
+declare -a arr=(".vim" ".vimrc" ".gvimrc" ".gitconfig")
 
 for i in "${arr[@]}"
 do
@@ -15,13 +15,3 @@ elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     ln -sf $CUR_PATH/.bashrc $HOME/.bashrc
 # elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then # WINDOWS
 fi
-
-# GIT
-git config --global alias.co checkout
-git config --global alias.ci commit
-git config --global alias.st status
-git config --global alias.br branch
-git config --global alias.cp cherry-pick
-git config --global alias.hist "log --pretty=format:'%h %ad | %s%d [%an]' --graph --date=short"
-git config --global alias.type 'cat-file -t'
-git config --global alias.dump 'cat-file -p'
