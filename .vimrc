a9f6f5bc3b52b4478cd75974a0c762a983095741
Shaohsuan Cheng <shaohsuan.cheng.tw@gmail.com>
2020-01-22 11:50:27 +0800

    Fix some issues

--- .vimrc
+++ .vimrc
@@ -20,3 +20,5 @@ python3 from powerline.vim import setup as powerline_setup
 python3 powerline_setup()
 python3 del powerline_setup
 
+set background=dark
+colorscheme PaperColor
--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 CUR_PATH=$(pwd)
 
-declare -a arr=(".sh_profile" ".bash_profile" ".bashrc" ".zshrc" ".vimrc" ".gvimrc" ".gitconfig" ".zonshrc")
+declare -a arr=(".sh_profile" ".bash_profile" ".bashrc" ".zshrc" ".vimrc" ".gvimrc" ".gitconfig" ".xonshrc")
 
 for i in "${arr[@]}"
 do
